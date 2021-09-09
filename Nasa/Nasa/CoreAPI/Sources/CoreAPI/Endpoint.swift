a47883982b0a763d9b4a80bba2431708b70d75c4
bahadirseyfi <bahadirsoftware@gmail.com>
2021-09-09 03:04:11 +0300

    Added Network Module

---
+++ Nasa/Nasa/CoreAPI/Sources/CoreAPI/Endpoint.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  
+//
+//
+
+public protocol Endpoint {
+    var baseUrl: String { get }
+    var path: String { get }
+    var method: HTTPMethod { get }
+    var parameters: [String: Any] { get }
+    var headers: [String: String] { get}
+}
+
+public extension Endpoint {
+    var headers: [String: String] { [:] }
+    var parameters: [String: Any] { [:] }
+    var url: String { "\(baseUrl)\(path)"}
+}
