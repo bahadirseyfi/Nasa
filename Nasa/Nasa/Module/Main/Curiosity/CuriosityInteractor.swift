f6e3f35b5396341dc2e9c343d1d82e6adfc92803
bahadirseyfi <bahadirsoftware@gmail.com>
2021-09-13 19:31:46 +0300

    Curiorsity Interactor Module

--- Nasa/Nasa/Module/Main/Curiosity/CuriosityInteractor.swift
+++ Nasa/Nasa/Module/Main/Curiosity/CuriosityInteractor.swift
@@ -5,17 +5,36 @@
 //  Created by bahadir on 13.09.2021.
 //
 
+import CoreAPI
 import Foundation
- 
+
 // ViewController -> presenter Interface
 protocol CuriosityInteractorInterface {
-    
+    func fetchPhotos(page: String, filter: String)
 }
+
+// CuriosityInteractorOutput -> presenter
+protocol CuriosityInteractorOutput: AnyObject {
+    func handlePhotoResult(_ result: PhotoResult)
+}
+
+typealias PhotoResult = Result<RoverModel, APIClientError>
 
 final class CuriosityInteractor {
     
+    private var networkManager: NetworkManager<EndpointItem> = NetworkManager()
+    
+    weak var output: CuriosityInteractorOutput?
+    
+    init(networkManager: NetworkManager<EndpointItem> = NetworkManager()) {
+        self.networkManager = networkManager
+    }
 }
 
 extension CuriosityInteractor: CuriosityInteractorInterface {
-    
+    func fetchPhotos(page: String, filter: String) {
+        networkManager.request(endpoint: .curiosity(page: page, filter: filter), type: RoverModel.self) { [weak self] (result) in
+            self?.output?.handlePhotoResult(result)
+        }
+    }
 }
